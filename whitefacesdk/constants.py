babf3a2b36eba71c91e212c5d6fc99721a6d8b9f
Wes Young <wes@barely3am.com>
2015-10-29 10:16:42 -0400

    adding env variables to constants

--- whitefacesdk/constants.py
+++ whitefacesdk/constants.py
@@ -1,13 +1,21 @@
+import os.path
 
 API_VERSION = 0
 
 REMOTE = 'https://whiteface.csirtgadgets.com/api'
+REMOTE = os.environ.get('WF_REMOTE', REMOTE)
 
 LOG_FORMAT = '%(asctime)s - %(levelname)s - %(name)s[%(lineno)s] - %(message)s'
 
 LIMIT = 500
+LIMIT = os.environ.get('WF_LIMIT', LIMIT)
+
 TIMEOUT = 300
+TIMEOUT = os.environ.get('WF_TIMEOUT', TIMEOUT)
 
+COLUMNS = "user,feed,thing,comments,protocol,portlist,tags,updated_at"
+COLUMNS = os.environ.get('WF_COLUMNS', COLUMNS)
+COLUMNS = COLUMNS.split(',')
 
-COLUMNS = ['user', 'feed', 'thing', 'comments', 'protocol', 'portlist', 'tags', 'updated_at']
 MAX_FIELD_SIZE = 30
+MAX_FIELD_SIZE = os.environ.get('WF_MAX_FIELD_SIZE', MAX_FIELD_SIZE)
