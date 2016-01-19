7f72aacba160956c9419f5723fdf42ddf2bece5f
Wes Young <wes@barely3am.com>
2016-01-18 19:38:33 -0500

    fixing env vars

--- csirtgsdk/constants.py
+++ csirtgsdk/constants.py
@@ -3,19 +3,19 @@ import os.path
 API_VERSION = 0
 
 REMOTE = 'https://csirtg.io/api'
-REMOTE = os.environ.get('WF_REMOTE', REMOTE)
+REMOTE = os.environ.get('CSIRTG_REMOTE', REMOTE)
 
 LOG_FORMAT = '%(asctime)s - %(levelname)s - %(name)s[%(lineno)s] - %(message)s'
 
 LIMIT = 500
-LIMIT = os.environ.get('WF_LIMIT', LIMIT)
+LIMIT = os.environ.get('CSIRTG_LIMIT', LIMIT)
 
 TIMEOUT = 300
-TIMEOUT = os.environ.get('WF_TIMEOUT', TIMEOUT)
+TIMEOUT = os.environ.get('CSIRTG_TIMEOUT', TIMEOUT)
 
 COLUMNS = "user,feed,thing,comments,protocol,portlist,portlist_src,tags,description,updated_at"
-COLUMNS = os.environ.get('WF_COLUMNS', COLUMNS)
+COLUMNS = os.environ.get('CSIRTG_COLUMNS', COLUMNS)
 COLUMNS = COLUMNS.split(',')
 
 MAX_FIELD_SIZE = 30
-MAX_FIELD_SIZE = os.environ.get('WF_MAX_FIELD_SIZE', MAX_FIELD_SIZE)
+MAX_FIELD_SIZE = os.environ.get('CSIRTG_MAX_FIELD_SIZE', MAX_FIELD_SIZE)
