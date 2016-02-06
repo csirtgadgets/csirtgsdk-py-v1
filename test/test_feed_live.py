8e17a9febb6e9f38032fadcadb27bbb12f503db3
Wes Young <wes@barely3am.com>
2016-02-06 12:39:55 -0500

    adding live test example

---
+++ test/test_feed_live.py
@@ -0,0 +1,37 @@
+import pytest
+import os
+from pprint import pprint
+from csirtgsdk.feed import Feed
+from csirtgsdk.client import Client
+
+CI_BUILD = os.environ.get('CI_BUILD', False)
+TOKEN = os.environ.get('CSIRTG_TOKEN', None)
+USER = os.environ.get('CSIRTG_USER', 'wes')
+REMOTE = os.environ.get('CSIRTG_REMOTE', 'https://csirtg.io/api')
+
+liveonly = pytest.mark.skipif(CI_BUILD is False, reason="CI_BUILD env var not set")
+
+@pytest.fixture
+def client():
+    return Client(
+        token=TOKEN,
+        remote=REMOTE
+    )
+
+@liveonly
+def test_create_feed(client):
+    if CI_BUILD:
+        f = Feed(client).new(USER, 'CI_BUILD_TEST', description='test build feed')
+
+        assert f['created_at']
+
+        f = Feed(client).remove(USER, 'CI_BUILD_TEST')
+
+        assert f == 200
+
+@liveonly
+def test_feed(client):
+    if CI_BUILD:
+        f = Feed(client)
+
+        assert f.client
