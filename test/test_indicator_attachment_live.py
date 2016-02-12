ab9c7a2a1577777e6cdd556d76b33bd6f00254fd
Gabriel Iovino <gabriel.iovino@internetidentity.com>
2016-02-12 17:07:27 -0500

    adding a test for an attachment that failed
    error: RuntimeError: Validation failed: Attachment content type is invalid, Attachment is invalid

--- test/test_indicator_attachment_live.py
+++ test/test_indicator_attachment_live.py
@@ -9,7 +9,7 @@ CI_BUILD = os.environ.get('CI_BUILD', False)
 TOKEN = os.environ.get('CSIRTG_TOKEN', None)
 USER = os.environ.get('CSIRTG_USER', 'wes')
 REMOTE = os.environ.get('CSIRTG_REMOTE', 'https://csirtg.io/api')
-FEED = 'CI_ATTACHMENT_TEST'
+FEED = os.environ.get('CSIRTG_FEED', 'CI_ATTACHMENT_TEST')
 
 liveonly = pytest.mark.skipif(CI_BUILD is False, reason="CI_BUILD env var not set")
 
@@ -56,6 +56,26 @@ def test_indicator_attachment_jar(client):
     r = i.submit()
     pprint(r)
     assert r['indicator']['indicator'] == '2097a35058b337aa4c8b3bafdb6ecc7b8dc4df5c'
+    assert r['indicator']['attachments'][0]['attachment']
+    assert r['indicator']['attachments'][0]['filesize']
+    assert r['indicator']['attachments'][0]['created_at']
+    
+
+@liveonly
+def test_indicator_attachment_docx(client):
+    f = Feed(client).new(USER, FEED, description='test build feed')
+
+    assert f['created_at']
+
+    i = Indicator(client, {
+        'user': USER,
+        'feed': FEED,
+        'attachment': 'samples/c..docx',
+        'comment': 'asdfasdfasdf'
+    })
+    r = i.submit()
+    pprint(r)
+    assert r['indicator']['indicator'] == '3758401d18b855f33777c5081f9a0c1836be44e3'
     assert r['indicator']['attachments'][0]['attachment']
     assert r['indicator']['attachments'][0]['filesize']
     assert r['indicator']['attachments'][0]['created_at']
