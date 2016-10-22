152952a47a566040e940ca47e0da0380296ddf97
Wes Young <wes@barely3am.com>
2016-10-22 10:56:30 -0400

    re-factoring for release pipleine

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
 from setuptools import setup, find_packages
+import os
+import sys
+import versioneer
 
 with open('requirements.txt') as f:
     reqs = f.read().splitlines()
 
-import versioneer
-versioneer.VCS = 'git'
-versioneer.versionfile_source = 'csirtgsdk/_version.py'
-versioneer.versionfile_build = 'csirtgsdk/_version.py'
-versioneer.tag_prefix = ''  # tags are like 1.2.0
-versioneer.parentdir_prefix = 'py-csirtgsdk-'  # dirname like 'myproject-1.2.0'
+
+# https://www.pydanny.com/python-dot-py-tricks.html
+if sys.argv[-1] == 'test':
+    test_requirements = [
+        'pytest',
+        'coverage',
+        'pytest_cov',
+    ]
+    try:
+        modules = map(__import__, test_requirements)
+    except ImportError as e:
+        err_msg = e.message.replace("No module named ", "")
+        msg = "%s is not installed. Install your test requirments." % err_msg
+        raise ImportError(msg)
+    os.system('py.test test -v --cov=csirtgsdk --cov-fail-under=25')
+    sys.exit()
 
 setup(
     name="csirtgsdk",
@@ -16,7 +29,7 @@ setup(
     cmdclass=versioneer.get_cmdclass(),
     description="CSIRTG Python SDK",
     long_description="CSIRTG Software Development Kit for Python",
-    url="https://github.com/csirtgadgets/py-csirtgsdk",
+    url="https://github.com/csirtgadgets/csirtgsdk-py",
     license='LGPL3',
     classifiers=[
         "Topic :: System :: Networking",
@@ -25,7 +38,7 @@ setup(
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
         "Programming Language :: Python"
     ],
-    keywords=['security'],
+    keywords=['network','security'],
     author="Wes Young",
     author_email="wes@csirtgadgets.org",
     packages=find_packages(),
